123c4b2315cd0772ac47fb552191d151f78c1adf
samillymacedo914-source <samillymacedo914@gmail.com>
2026-09-25 16:51:50 -0300

    Update ProjectRepository.java

--- DevShowcase-API/src/main/java/com/devshowcase/api/repository/ProjectRepository.java
+++ DevShowcase-API/src/main/java/com/devshowcase/api/repository/ProjectRepository.java
@@ -1,1 +1,14 @@
-package com.devshowcase.api.repository; import com.devshowcase.api.entity.Project; import org.springframework.data.jpa.repository.JpaRepository; public interface ProjectRepository extends JpaRepository<Project,Long>{}
+package com.devshowcase.api.repository;
+
+import com.devshowcase.api.entity.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    Page<Project> findDistinctByTechnologies_Id(
+            Long technologyId,
+            Pageable pageable
+    );
+}
