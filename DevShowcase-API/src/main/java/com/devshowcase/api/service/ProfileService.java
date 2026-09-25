8d73027fe46b14087da1c6b386dc62a7d096d09f
samillymacedo914-source <samillymacedo914@gmail.com>
2026-09-25 17:38:35 -0300

    Create ProfileService.java

---
+++ DevShowcase-API/src/main/java/com/devshowcase/api/service/ProfileService.java
@@ -0,0 +1,39 @@
+package com.devshowcase.api.service;
+
+import com.devshowcase.api.dto.DTOs.*;
+import com.devshowcase.api.entity.Profile;
+import com.devshowcase.api.repository.ProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class ProfileService {
+
+    private final ProfileRepository r;
+
+    public ProfileService(ProfileRepository r) {
+        this.r = r;
+    }
+
+    public ProfileResponse create(ProfileCreate d) {
+        Profile p = new Profile();
+        p.setName(d.name().trim());
+        p.setEmail(d.email().trim());
+        return out(r.save(p));
+    }
+
+    public ProfileResponse get(Long id) {
+        return out(r.findById(id)
+                .orElseThrow(() ->
+                        new NoSuchElementException("Perfil não encontrado: " + id)));
+    }
+
+    private ProfileResponse out(Profile x) {
+        return new ProfileResponse(
+                x.getId(),
+                x.getName(),
+                x.getEmail()
+        );
+    }
+}
